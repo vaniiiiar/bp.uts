f28a44ee1917a3662a05f2b853facb0d7c9ac082
vaniiiiar <vaniarahmawati252@student.esaunggul.ac.id>
2024-05-13 22:57:47 +0700

    fresh commit

---
+++ bp.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+string lahir;
+int main(){
+    cout << " Hari Lahir Kamu : " << endl;
+    cin >> lahir;
+
+    cout << " Hari Lahir Kamu " << lahir << endl;
+
+    return 0;
+}
